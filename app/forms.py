7b9cc23fc4c512e1b84234a14d2ba12d69860325
Talison <talison@outlook.com>
2024-08-29 06:22:36 -0300

    fixed typo

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
@@ -14,4 +14,4 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
-    recaptcha = RecaptchaField()
+
